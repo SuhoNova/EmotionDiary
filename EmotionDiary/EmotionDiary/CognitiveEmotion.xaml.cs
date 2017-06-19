7041866aaf727dace325ae690e3bb34d3247328d
Alex <suhonova@gmail.com>
2017-06-20 02:42:46 +1200

    Trying to fix content processing after api call

--- EmotionDiary/EmotionDiary/CognitiveEmotion.xaml.cs
+++ EmotionDiary/EmotionDiary/CognitiveEmotion.xaml.cs
@@ -1,16 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
+using EmotionDiary.Model;
+using Newtonsoft.Json;
 using Plugin.Media;
 using Plugin.Media.Abstractions;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
+
 namespace EmotionDiary
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class CognitiveEmotion : ContentPage
 	{
 		public CognitiveEmotion ()
@@ -47,7 +50,58 @@ namespace EmotionDiary
 	            return file.GetStream();
 	        });
 
-	        file.Dispose();
-        }
-	}
+            EmotionLabel.Text = "";
+
+            await EvaluateEmotionRequest(file);
+	    }
+
+	    static byte[] GetImageAsByteArray(MediaFile file)
+	    {
+	        var stream = file.GetStream();
+	        BinaryReader binaryReader = new BinaryReader(stream);
+	        return binaryReader.ReadBytes((int)stream.Length);
+	    }
+
+	    async Task EvaluateEmotionRequest(MediaFile file)
+	    {
+	        var client = new HttpClient();
+
+	        client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", "1c36e4946d994473b89f5edea07f11f6");
+
+	        string url = "https://westus.api.cognitive.microsoft.com/emotion/v1.0/recognize?";
+
+	        HttpResponseMessage response;
+
+            byte[] byteData = GetImageAsByteArray(file);
+
+	        using (var content = new ByteArrayContent(byteData))
+	        {
+
+	            content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+	            response = await client.PostAsync(url, content);
+
+
+	            if (response.IsSuccessStatusCode)
+	            {
+	                var responseString = await response.Content.ReadAsStringAsync();
+
+                    var emotions = JsonConvert.DeserializeObject<RootObject>(responseString);
+
+                    //double max = responseModel.Predictions.Max(m => m.Probability);
+	                var scores = emotions.scores;
+	                var highestScore = scores.Values.Max(s => s);
+	                var highestEmotion = scores.FirstOrDefault(s => s.Value == highestScore).Key;
+
+                    //TagLabel.Text = (max >= 0.5) ? "Hotdog" : "Not hotdog";
+	                EmotionLabel.Text = "You feel " + highestEmotion;
+
+                }
+
+	            //Get rid of file once we have finished using it
+	            file.Dispose();
+	        }
+	    }
+
+
+    }
 }
---
+++ EmotionDiary/EmotionDiary/Model/EmotionModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmotionDiary.Model
+{
+
+        public class FaceRectangle
+        {
+            public int Top { get; set; }
+            public int Left { get; set; }
+            public int Width { get; set; }
+            public int Height { get; set; }
+        }
+
+
+        public class RootObject
+        {
+            public FaceRectangle FaceRectangle { get; set; }
+            public IDictionary<string, double> scores { get; set; }
+        }
+
+}
